c21a26f4f97024eb5c36ead366edd2c5e4371022
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-22 05:52:38 +0600

    ✨ feat: create nav

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Button from "./ui/button/button.svelte";
+</script>
+
+<nav class="flex items-center p-3 justify-between border-b">
+  <div>
+    <a href="/" class="font-bold px-4 hover:text-gray-700">Home</a>
+  </div>
+  <div class="flex items-center gap-2">
+    <a href="/api/auth/login" class="text-sm px-4 hover:text-gray-700">
+      <Button>Login</Button>
+    </a>
+  </div>
+</nav>
