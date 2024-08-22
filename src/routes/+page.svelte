02c33f34d27d682b1961171adf60b079c4dd3344
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-22 06:25:09 +0600

    ✨ feat: get auth status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    export let data;
+    let { isAuthenticated } = data;
+    console.log(isAuthenticated)
+</script>
+
+
+{#if isAuthenticated}
+	<p>Welcome dear user</p>
+{:else}
+<p>You are not logged in</p>
+{/if}
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
