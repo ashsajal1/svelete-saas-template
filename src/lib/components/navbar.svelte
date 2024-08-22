8c855baab5f7a5ca80455acc75dbb45cf2bc902f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-22 06:38:22 +0600

    ✨ feat: add data prop

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,5 +1,6 @@
 <script>
   import Button from "./ui/button/button.svelte";
+  export let data;
 </script>
 
 <nav class="flex items-center p-3 justify-between border-b">
@@ -7,8 +8,14 @@
     <a href="/" class="font-bold px-4 hover:text-gray-700">Home</a>
   </div>
   <div class="flex items-center gap-2">
-    <a href="/api/auth/login" class="text-sm px-4 hover:text-gray-700">
-      <Button>Login</Button>
-    </a>
+    {#if data.isAuthenticated}
+      <a href="/api/auth/logout" class="text-sm px-4 hover:text-gray-700">
+        <Button>Logout</Button>
+      </a>
+    {:else}
+      <a href="/api/auth/login" class="text-sm px-4 hover:text-gray-700">
+        <Button>Login</Button>
+      </a>
+    {/if}
   </div>
 </nav>
