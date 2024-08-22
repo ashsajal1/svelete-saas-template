636c49d0f56737b910fd58db79fde97c7bf82bab
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-22 07:21:30 +0600

    ✨ feat: get user name

--- src/routes/profile/[id]/+page.svelte
+++ src/routes/profile/[id]/+page.svelte
@@ -1,7 +1,13 @@
 <script>
-    export let data;
+  export let data;
 </script>
 
 <div>
+  {#if data.isAuthenticated}
+    <p>Welcome dear user</p>
     {data.id}
+    {data.name}
+  {:else}
+    <p>Log in</p>
+  {/if}
 </div>
